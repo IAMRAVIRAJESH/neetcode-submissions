976ec9b54db12a52dd3f25225864c72b03467f77
IAMRAVIRAJESH <77628515+IAMRAVIRAJESH@users.noreply.github.com>
2026-08-07 14:36:26 +0530

    Add: gas-station - submission-4

---
+++ Data Structures & Algorithms/gas-station/submission-4.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} gas
+     * @param {number[]} cost
+     * @return {number}
+     */
+    canCompleteCircuit(gas, cost) {
+        let tg=0, tc=0, n = gas.length, curr = 0, start = 0;
+
+        for(let i=0; i<n; i++) {
+            tg += gas[i];
+            tc += cost[i];
+            curr += gas[i]-cost[i];
+
+            if(curr < 0) {
+                start = i+1;
+                curr = 0;
+            }
+        }
+        if(tg<tc) return -1;
+
+        return start;
+    }
+}
